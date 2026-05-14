bb2a9f4709e226d16e7f4ec57f6eb7a5810613f6
minhhhhh-affk <minhpahe180198@fpt.edu.vn>
2026-05-14 11:10:38 +0700

    Delete crawler_project directory

--- crawler_project/main.py
+++
@@ -1,9 +0,0 @@
-from database import setup_database
-from core import run_crawlers
-from sites.sites import ALL_SITES
-
-# Setup database tables if not exists
-setup_database()
-
-# Run all crawlers at the same time
-run_crawlers(ALL_SITES)
